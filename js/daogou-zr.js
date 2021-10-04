cb74321da195e4867ddd37104e4fe2bcede6f7c9
yyecheng <2592549896@qq.com>
2021-10-04 22:54:45 +0800

    daogou.html

---
+++ js/daogou-zr.js
@@ -0,0 +1,18 @@
+var oajax = new XMLHttpRequest() || new ActiveXObject('Microsoft.XMLHTTP');
+oajax.open('get','http://192.168.31.136:3000/guid/hot',true)
+oajax.send() ;
+oajax.onreadystatechange = function(){
+ if(oajax.readyState == 4 ){
+   if(oajax.status == 200){
+    var a = eval('('+oajax.responseText+')')
+    console.log(a);
+    fn(a)
+   }
+ }
+}
+function fn(a){
+    var html = template('first',{
+        value:a
+    });
+    document.getElementsByClassName('dg-box')[0].innerHTML = html;
+}
